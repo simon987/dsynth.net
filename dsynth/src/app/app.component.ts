e0d44c630dbd60c1ff4922542677d184093d804d
drone <admin@dsynth.net>
2020-03-12 10:49:18 -0400

    i18n basic setup

--- dsynth/src/app/app.component.ts
+++ dsynth/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import {Component} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: []
 })
 export class AppComponent {
-    title = 'dsynth';
+
+    constructor(private translate: TranslateService) {
+
+        translate.addLangs([
+            'en',
+            'fr',
+            'ru'
+        ]);
+
+        translate.setDefaultLang('en');
+    }
+
+    langList: any[] = [
+        {lang: 'fr', display: 'Français'},
+        {lang: 'en', display: 'English'},
+        {lang: 'ru', display: 'Русский'},
+    ];
+
+    langChange(lang: any) {
+        this.translate.use(lang.lang);
+    }
 }
--- dsynth/src/app/app.module.ts
+++ dsynth/src/app/app.module.ts
@@ -4,6 +4,17 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {MatButtonModule} from "@angular/material/button";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+export function createTranslateLoader(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
     declarations: [
@@ -12,7 +23,20 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatMenuModule,
+        MatIconModule,
+        MatButtonModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+                loader: {
+                    provide: TranslateLoader,
+                    useFactory: (createTranslateLoader),
+                    deps: [HttpClient]
+                }
+            }
+        ),
     ],
     providers: [],
     bootstrap: [AppComponent]
